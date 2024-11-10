e835391bb79c747f6e0d050907ed6463b65d7f9c
Shunke4444 <jihadtejam867@gmail.com>
2024-11-10 11:24:20 +0800

    fix interestpage responsive

--- src/components/interestpage.jsx
+++ src/components/interestpage.jsx
@@ -2,9 +2,9 @@ import { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import InterestList from './interestList';
-import Credits from '../assets/interests/Credits.webp'
-import Persona from '../assets/interests/persona3.jpg'
-import Music from '../assets/interests/Spotify.png'
+import Credits from '../assets/interests/Credits.webp';
+import Persona from '../assets/interests/persona3.jpg';
+import Music from '../assets/interests/Spotify.png';
 import Grid from '../assets/GridDesign.png';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -19,7 +19,8 @@ const InterestPage = () => {
 
     const getScrollAmount = () => {
       let racesWidth = races.scrollWidth;
-      return -(racesWidth - window.innerWidth);
+      let screenWidth = window.innerWidth;
+      return screenWidth < 768 ? -(racesWidth - screenWidth / 1.2) : -(racesWidth - screenWidth);
     };
 
     const tween = gsap.to(races, {
@@ -31,7 +32,7 @@ const InterestPage = () => {
     const scrollTrigger = ScrollTrigger.create({
       trigger: racesWrapper,
       start: 'top top',
-      end: () => `+=${getScrollAmount() * -1}`,
+      end: () => `+=${Math.abs(getScrollAmount())}`,
       pin: true,
       animation: tween,
       scrub: 1,
@@ -39,64 +40,69 @@ const InterestPage = () => {
       markers: false,
     });
 
+    const handleResize = () => {
+      tween.vars.x = getScrollAmount();
+      scrollTrigger.refresh();
+    };
+
+    window.addEventListener("resize", handleResize);
     return () => {
       scrollTrigger.kill();
       tween.kill();
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-<main className='bg-heroDarkBlue h-screen w-screen'>
-  <section className="relative overflow-hidden bg-heroDarkBlue h-[100vh]" ref={racesWrapperRef}>
-    <div className='min-h-[8rem] w-screen p-0 ml-5 bg-heroDarkBlue flex flex-col items-start justify-center'>
-      <h1 className='text-[8rem] font-customFont font-bold text-white'>MY INTERESTS</h1>
-    </div>
-    <hr className='bg-white h-[1px] w-screen m-0'/>
+    <main className='bg-heroDarkBlue h-screen w-screen'>
+      <section className="relative overflow-hidden bg-heroDarkBlue h-[100vh]" ref={racesWrapperRef}>
+        <div className='min-h-[8rem] w-screen p-0 ml-5 bg-heroDarkBlue flex flex-col items-start justify-center'>
+          <h1 className='text-[4rem] md:text-[8rem] font-customFont font-bold text-white'>MY INTERESTS</h1>
+        </div>
+        <hr className='bg-white h-[1px] w-screen m-0'/>
 
-    <article className="inline-flex whitespace-nowrap" ref={racesRef}>
-      <div className='h-screen w-[500px] bg-cover bg-center' style={{backgroundImage: `url(${Grid})`}}></div>
-
-            
-      <InterestList 
-      img={Persona} 
-      title='Persona 3 Reload'
-      sub= {`Persona 3 is a Japanese role-playing game where players assume the 
-role of a high school student who joins the 
+        <article className="inline-flex whitespace-nowrap" ref={racesRef}>
+          <div className='h-screen w-[300px] md:w-[500px] bg-cover bg-center' style={{backgroundImage: `url(${Grid})`}}></div>
+              
+          <InterestList 
+            img={Persona} 
+            title='Persona 3 Reload'
+            sub={`Persona 3 is a Japanese role-playing game 
+where players assume the role of a high school student who joins the 
 Specialized Extracurricular Execution Squad (SEES).`}
-      useMap= '#persona'
-      areas={[
-        { shape: 'rect', coords: '3,3,1597,897', link: '/persona' },
-      ]}
-      />
-      
-      <InterestList 
-      img={Music} 
-      title='Music'
-      sub= {`I listen to a lot of random music.
-Really as long as it sounds good Id listen to it`}
-      useMap= '#music'
-      openInNewTab='_"blank"'
-      areas={[
-        { shape: 'rect', coords: '8,2,729,367', link: "https://open.spotify.com/playlist/5XbPSn5YCADKgGeHksDBbn?si=5a333cb3367a4191" },
-      ]}
-      />
-      <InterestList
-      img = {Credits}
-      title= 'Credits'
-      sub='These are the sources I&apos;ve used!'
-      useMap='#credits'
-      areas={[
-        { shape: 'rect', coords: '8,13,1263,710', link: '/credits'}
-      ]}
-      />
-      
-
-    </article>
-  </section>
-</main>
-
+            useMap='#persona'
+            areas={[
+              { shape: 'rect', coords: '3,3,1597,897', link: '/persona' },
+            ]}
+            className="md:w-[20rem] w-[15rem] text-[1rem] md:text-[1.5rem]"
+          />
+          
+          <InterestList 
+            img={Music} 
+            title='Music'
+            sub={`I listen to a lot of random music. 
+Really, as long as it sounds good, I'd listen to it.`}
+            useMap='#music'
+            openInNewTab='_blank'
+            areas={[
+              { shape: 'rect', coords: '8,2,729,367', link: "https://open.spotify.com/playlist/5XbPSn5YCADKgGeHksDBbn?si=5a333cb3367a4191" },
+            ]}
+            className="md:w-[20rem] w-[15rem] text-[1rem] md:text-[1.5rem]"
+          />
+          <InterestList
+            img={Credits}
+            title='Credits'
+            sub="These are the sources I've used!"
+            useMap='#credits'
+            areas={[
+              { shape: 'rect', coords: '8,13,1263,710', link: '/credits' }
+            ]}
+            className="md:w-[20rem] w-[15rem] text-[1rem] md:text-[1.5rem]"
+          />
+        </article>
+      </section>
+    </main>
   );
 };
 
 export default InterestPage;
- 
